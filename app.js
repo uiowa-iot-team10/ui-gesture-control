b66b8eb593ea786123995432f942b64150ae64df
Mert Erdemir <mert.erdemir@gmail.com>
2021-03-25 11:27:03 +0000

    node-ble working version (does not have automatic MAC address finding)

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { createBluetooth } = require( 'node-ble' );
 const express    = require('express');
 const app        = express();
 var EventEmitter = require('events');
@@ -7,6 +8,18 @@ events.setMaxListeners(20);
 
 var SCRIPT_PORT  = 9999;
 var CLIENT_PORT  = 80;
+
+var GESTURES = {
+    0: "UP",
+    1: "DOWN",
+    2: "LEFT",
+    3: "RIGHT"
+};
+
+const ARDUINO_BLUETOOTH_ADDR       = '5A:6B:31:73:71:00';
+const DATA_SERVICE_UUID            = '19B10010-E8F2-537E-4F6C-D104768A1214';
+const GYROX_CHARACTERISTIC_UUID    = '19B10011-E8F2-537E-4F6C-D104768A1214';
+const MOVEMENT_CHARACTERISTIC_UUID = '19B10012-E8F2-537E-4F6C-D104768A1214';
 
 var http1 = require('http').createServer(app);
 var http2 = require('http').createServer(app);
@@ -27,6 +40,43 @@ app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
+async function setBLE() {
+    // Reference the BLE adapter and begin device discovery...
+    const { bluetooth, destroy } = createBluetooth();
+    const adapter = await bluetooth.defaultAdapter();
+    const discovery =  await adapter.startDiscovery();
+    console.log( '[LOG] discovering...' );
+
+    // Attempt to connect to the device with specified BT address
+    const device = await adapter.waitDevice( ARDUINO_BLUETOOTH_ADDR.toUpperCase() );
+    console.log( '[LOG] found device. attempting connection...' );
+    await device.connect();
+    console.log( '[LOG] connected to device!' );
+
+    // Get references to the desired UART service and its characteristics
+    const gattServer = await device.gatt();
+    const dataService = await gattServer.getPrimaryService( DATA_SERVICE_UUID.toLowerCase() );
+    const gyroXChar = await dataService.getCharacteristic( GYROX_CHARACTERISTIC_UUID.toLowerCase() );
+    const movementChar = await dataService.getCharacteristic( MOVEMENT_CHARACTERISTIC_UUID.toLowerCase() );
+
+    // Register for notifications on the RX characteristic
+    await movementChar.startNotifications( );
+
+    // Callback for when data is received on RX characteristic
+    movementChar.on( 'valuechanged', buffer =>
+    {
+        console.log('Data is received from Arduino: ' + GESTURES[buffer[0]]);
+        events.emit("gesture", GESTURES[buffer[0]]);
+    });
+}
+
+setBLE().then((ret) =>
+{
+    if (ret) console.log( ret );
+}).catch((err) =>
+{
+    if (err) console.error( err );
+});
 
 // Socket stuff
 csio.on('connection', function(socket){
